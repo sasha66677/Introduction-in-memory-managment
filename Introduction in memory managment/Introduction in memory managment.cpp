f990f6997de0549fa7d4a8a6165bd96e7c2fd805
Alexander <123123salo21082018@gmail.com>
2023-03-24 20:32:20 +0300

    add structures showing alignment in memory

--- Introduction in memory managment/Introduction in memory managment.cpp
+++ Introduction in memory managment/Introduction in memory managment.cpp
@@ -1,20 +1,42 @@
-﻿// Introduction in memory managment.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+struct Align1 {
+    char number0 : 3;
+    char number1 : 3;
+    char number2 : 3;
+    char number3 : 3;
+    char number4 : 3;
+    //{7,7,7,7,7}
+    //111111001111110011100000
+    //sizeof() = 3
+};
+
+struct Align2
+{
+    char a;     // 1 byte
+    int b;      // 4 bytes
+    short c;    // 2 bytes
+    char d;     // 1 byte
+    //if max vals:
+    //FF 000000 FFFFFFFF FFFF FF 00 
+    // a               b    c  d
+};
+
+// Shows the actual memory layout
+struct Align3
+{
+    char a;            // 1 byte
+    char _pad0[3];     // padding to put 'b' on 4-byte boundary
+    int b;             // 4 bytes
+    short c;           // 2 bytes
+    char d;            // 1 byte
+    char _pad1[1];     // padding to make sizeof(x_) multiple of 4
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Align1 size: " << sizeof(Align1) << '\n';
+    std::cout << "Align2 size: " << sizeof(Align2) << '\n';
+    std::cout << "Align3 size: " << sizeof(Align3) << '\n';
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Introduction in memory managment
+Write a program that demonstrates the order of variables in memory and the effect of alignment on the size of structures.
+
+Create a function that takes a pointer to an array of structures allocated dynamically and determine the size of the array inside the function (using compiler-specific hacks for accessing overhead information) using the pointer and pointer arithmetic.
+The function will have to explicitly call the destructor on each element of the array.
