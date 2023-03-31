9d2b7b8df7736220ebeb6f55b32cd0e5b8f1cf4e
Alexander <123123salo21082018@gmail.com>
2023-03-31 18:54:08 +0300

    implement delet(T ptr) and length(T ptr) functions

--- Introduction in memory managment/Introduction in memory managment.cpp
+++ Introduction in memory managment/Introduction in memory managment.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include <iomanip>
+#include <iostream>
 
 struct Align1 {
     char number0 : 3;
@@ -20,23 +21,48 @@ struct Align2
     //if max vals:
     //FF 000000 FFFFFFFF FFFF FF 00 
     // a               b    c  d
+    //sizeof() = 12
 };
 
 // Shows the actual memory layout
 struct Align3
 {
-    char a;            // 1 byte
-    char _pad0[3];     // padding to put 'b' on 4-byte boundary
-    int b;             // 4 bytes
-    short c;           // 2 bytes
-    char d;            // 1 byte
-    char _pad1[1];     // padding to make sizeof(x_) multiple of 4
+    char a{};            // 1 byte
+    char _pad0[3]{};     // padding to put 'b' on 4-byte boundary
+    int b{};             // 4 bytes
+    short c{};           // 2 bytes
+    char d{};            // 1 byte
+    char _pad1[1]{};     // padding to make sizeof(x_) multiple of 4
+    ~Align3() {
+        std::cout << "destuctor Align3\n";
+    }
+    //sizeof() = 12
 };
 
+template <typename T>
+size_t length(T arr) {
+    auto parr = (size_t*)(arr)-1;
+    size_t size1 = *(parr);
+    return size1;
+}
+
+//template <typename T>
+void delet(Align3*& arr) {
+    int size = length(arr);
+    for (int i = 0; i < size; ++i) {
+        (*arr).~Align3();
+        arr++;
+    }
+    arr = nullptr;
+}
 int main()
 {
-    std::cout << "Align1 size: " << sizeof(Align1) << '\n';
-    std::cout << "Align2 size: " << sizeof(Align2) << '\n';
-    std::cout << "Align3 size: " << sizeof(Align3) << '\n';
+    //std::cout << "Align1 size: " << sizeof(Align1) << '\n';
+    //std::cout << "Align2 size: " << sizeof(Align2) << '\n';
+    //std::cout << "Align3 size: " << sizeof(Align3) << '\n';
+
+
+    auto arr = new Align3[3]{};
+    delet(arr);
 
 }
